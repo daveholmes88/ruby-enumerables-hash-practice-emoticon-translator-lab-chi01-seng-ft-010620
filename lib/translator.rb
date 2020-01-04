6393a6be1de09f12c03f86830bd081bed636d6d2
David Holmes <phoenix1988@gmail.com>
2020-01-04 19:47:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(emoticon_lab, emoticon)
   hash = load_library(emoticon_lab)
   hash.each do |key, value|
     value.each do |attribute|
-      binding.pry 
+      binding.pry
       if emoticon == attribute[0]
         return attribute[1]
       else
