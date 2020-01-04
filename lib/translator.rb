d50fff49735e578d3a5e9873f8dfde064b23131e
David Holmes <phoenix1988@gmail.com>
2020-01-04 20:09:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,8 +22,9 @@ def get_japanese_emoticon(emoticon_lab, emoticon)
       value.each do |attribute|
         if emoticon == attribute[0]
           return attribute[1]
-        else
-          "Sorry, that emoticon was not found"
+          if emoticon == nil
+            "Sorry, that emoticons was not found"
+          end 
         end
       end
     end
@@ -37,9 +38,7 @@ def get_english_meaning(emoticon_lab, emoticon)
       value.each do |attribute|
         if emoticon == attribute[0]
           return attribute[1]
-          if emoticon == nil
-            "Sorry, that emoticon was not found"
-          end
+        else
         end
       end
     end
